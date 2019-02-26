96a6bad88cf5826eebf2b37dd6a2a6736ba47412
ZhengmingPan <35482497+ZhengmingPan@users.noreply.github.com>
2019-02-26 15:04:54 +0800

    DevTools热部署控件

---
+++ spring-boot-devtools/src/main/java/com/springboot2/devtools/SpringBootDevtoolsApplication.java
@@ -0,0 +1,12 @@
+package com.springboot2.devtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDevtoolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDevtoolsApplication.class, args);
+    }
+}
